e1c8b0ac18a148e3acd27614d6c75683406e27a2
vanessavps <vanessa.vps29@gmail.com>
2022-04-20 12:52:35 +1200

    First commit

---
+++ src/test/java/com/charge/ecommerce/ChargeEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charge.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
